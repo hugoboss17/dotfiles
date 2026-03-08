d83e3c60d32752d895b38f9415efe4938a6a7f07
Hugo Rodrigues <hugodfrodrigues@gmail.com>
2026-03-08 23:42:04 +0000

    chore: remove unused files and add .DS_Store to gitignore

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -1,3 +1,8 @@
+# PATH
+fish_add_path /opt/homebrew/bin
+fish_add_path $HOME/.config/herd-lite/bin
+fish_add_path /opt/metasploit-framework/bin
+
 if status is-interactive
 # Commands to run in interactive sessions can go here
 end
